763e9515dee6760f28921999b33006f2eb22c315
karnavDesai99 <45823615+karnavDesai99@users.noreply.github.com>
2020-03-04 07:34:59 +0530

    Create smart_trash_bin.py

---
+++ smart_trash_bin.py
@@ -0,0 +1,63 @@
+#libraries
+import RPi.GPIO as GPIO
+import time
+from firebase import firebase
+
+#GPIO Mode (BOARD/BCM)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+#set GPIO Pins
+GPIO_TRIGGER = 18
+GPIO_ECHO = 24
+
+#set GPIO direction (IN/OUT)
+GPIO.setup(GPIO_TRIGGER, GPIO.OUT)
+GPIO.setup(GPIO_ECHO, GPIO.IN)
+
+#Link Firebase
+firebase = firebase.FirebaseApplication(<your firebase project URL>, None)
+
+def distance():
+    #set Trigger yo HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+    
+    #set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+    
+    StartTime = time.time()
+    StopTime = time.time()
+    
+    #save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+        
+    #save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+    
+    #time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    #multiply with the sonic speed (34300 cm/s)
+    #and divide by 2, because sound will take time to reach and then reflect back
+    distance = (TimeElapsed*34300)/2
+    
+    return distance
+
+def upload(dist):
+   data = { 'msg' : 'FULL','dist' : dist }
+   firebase.post('Example-Bin-420-69', data)
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            dist = distance()
+            print ("Measured Distance = %.1f cm" %dist)
+            if (dist <= 4.0):
+                upload(dist)
+            time.sleep(5)
+            
+            #Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
